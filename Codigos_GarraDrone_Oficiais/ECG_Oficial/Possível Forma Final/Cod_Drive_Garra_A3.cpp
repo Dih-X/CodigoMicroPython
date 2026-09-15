b709595555d72de61bb92674854626cc0cc30c38
Diogo <d.alves.117.x@gmail.com>
2026-09-15 13:13:49 +0000

    Add de codigo do drive e mudança de pastas

---
+++ Codigos_GarraDrone_Oficiais/ECG_Oficial/Possível Forma Final/Cod_Drive_Garra_A3.cpp
@@ -0,0 +1,40 @@
+
+/*
+
+Requer a biblioteca ax12.h
+Pacote (ArbotiX Sensor and Servo Library) da Trossen Robotics, disponível em:
+https://www.trossenrobotics.com/productdocs/Arbotix/Arbotix%20Sensor%20and%20Servo%20Library.pdf
+
+Ligação(nesta praca(placa)):
+D0/D1 (RX/TX) -> ESP32 (via divisor de tensão no sentido ArbotiX -> ESP32)
+Servo AX-12A  -> um dos 3 conectores brancos (AX/S1 bus)
+
+*/
+
+#include <ax12.h>
+
+#define GRIPPER_ID 1      // ID do servo
+#define POS_ABERTA 200    // Posição aberta (0-1023)
+#define POS_FECHADA 800   // Posição fechada (0-1023)
+#define VELOCIDADE 100    // Velocidade de movimento (aparentemente vai de 0-1023 tbm)
+
+void setup(){
+    Serial.begin(9600);
+    ax12Init(1000000);
+
+    SetSpeed(GRIPPER_ID, VELOCIDADE);
+    SetPosition(GRIPPER_ID, POS_ABERTA);
+}
+
+void loop(){
+    if (Serial.available()){
+        char cmd = Serial.read();
+        
+        if (cmd == 'O'){
+            SetPosition(GRIPPER_ID, POS_ABERTA);
+        }
+        else if (cmd == 'F'){
+            SetPosition(GRIPPER_ID, POS_FECHADA);
+        }
+    }
+}
